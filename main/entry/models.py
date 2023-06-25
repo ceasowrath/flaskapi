3973c95259d614878eb3a1db49095dbf31ad5344
ceasowrath <102916210+ceasowrath@users.noreply.github.com>
2023-06-25 10:17:55 +0530

    Add files via upload

---
+++ main/entry/models.py
@@ -0,0 +1,55 @@
+"""Application Models"""
+import bson, os, datetime
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+load_dotenv()
+
+DATABASE_URL=os.environ.get('DATABASE_URL') or 'mongodb+srv://ceasowrath:Rizq7tkT2RmmWn37@cluster0.t1ez4iv.mongodb.net/gateentry?retryWrites=true&w=majority'
+client = MongoClient(DATABASE_URL)
+db = client.gateentry
+
+
+class Entry:
+    """Entry Model"""
+    def __init__(self):
+        return
+
+    def create(self,userid, gateEntryNumber,created_name, recieving_details="", shipping_details={}, customer_details="", items_list=[],comments=""):
+        if len(items_list) == 0:
+            return
+        """Create a new gate entry"""
+        new_entry = db.entries.insert_one(
+            {
+                "uniqueId": gateEntryNumber,
+                "recieving_details": recieving_details,
+                "shipping_details": shipping_details,
+                "customer_details": customer_details,
+                "items_list": items_list,
+                "created_name": created_name,
+                "comments": comments,
+                "createdBy": userid,
+                "timestamp": datetime.datetime.now().isoformat()
+            }
+        )
+        return self.get_by_id(new_entry.inserted_id)
+
+    def get_all(self):
+        """Get all entries"""
+        entries = db.entries.find({})
+            
+        return [{**entry, "_id": str(entry["_id"])} for entry in entries]
+
+    def get_by_id(self, entry_id):
+        """Get a entry by id"""
+        entry = db.entries.find_one({"_id": bson.ObjectId(entry_id)})
+        if not entry:
+            return
+        entry["_id"] = str(entry["_id"])
+        return entry
+
+    def get_by_user_id(self, user_id):
+        """Get all entries created by a user"""
+        entries = db.entries.find({"user_id": user_id})
+        
+        return [{**entry, "_id": str(entry["_id"])} for entry in entries]
---
+++ main/entry/routes.py
@@ -0,0 +1,103 @@
+import datetime
+from flask import Blueprint, jsonify, request
+from flask import current_app as app
+from main.auth import token_required
+from main.customer.models import Customer
+from main.entry.models import Entry
+from main.validation import  validate_add_entry
+
+entry_blueprint = Blueprint("entry", __name__)
+
+@entry_blueprint.route("/", methods=["GET"])
+@token_required
+def get_all_entries(current_user):
+    return jsonify({
+        "message": "successfully retrieved user profile",
+        "data": [{**entry, "createdBy": None} for entry in Entry().get_all()]
+    })
+
+
+@entry_blueprint.route("/add", methods=["POST"])
+@token_required
+def addEntry(current_user):
+    try:
+        data = request.json
+        if not data:
+            return {
+                "message": "Please provide entry details",
+                "data": None,
+                "error": "Bad request"
+            }, 400
+        # validate input
+        is_validated = validate_add_entry(created_by=current_user['_id'], **request.json)
+        if is_validated is not True:
+            return dict(message='Invalid data', data=None, error=is_validated), 400
+        
+        Customer().create(
+            name=request.json['customer_details']['name'],
+            address=request.json['customer_details']['address']
+        )
+
+        entries =Entry().get_all()
+ 
+        gateEntryNumber = "QBD/"
+        str_value = str(len(entries) + 1)
+        pad = "0000"
+        ans = pad[:len(pad) - len(str_value)] + str_value
+
+        today = datetime.datetime.now()
+        if today.month > 2:
+            gateEntryNumber += str(today.year) + "-" + str(today.year + 1)[-2:]
+        else:
+            gateEntryNumber += str(today.year - 1) + "-" + str(today.year)[-2:]
+
+        gateEntryNumber += "/" +today.strftime("%b").upper() + "/" +ans
+
+        entry = Entry().create(userid=current_user['_id'], created_name= current_user['name'], gateEntryNumber=gateEntryNumber, **request.json, )
+        if entry:
+            try:
+                return {
+                    "message": "Successfully created a entry",
+                    "data": entry
+                }
+            except Exception as e:
+                return {
+                    "error": "Something went wrong",
+                    "message": str(e)
+                }, 500
+        return {
+            "message": "Error creating entry, form validation failed",
+            "data": None,
+            "error": "Validation failed"
+        }, 404
+    except Exception as e:
+        return {
+                "message": "Something went wrong!",
+                "error": str(e),
+                "data": None
+        }, 500
+    
+
+# @entry_blueprint.route("/update", methods=["PUT"])
+# @token_required
+# def updateEntry(current_user):
+#     try:
+#         user = request.json
+#         if user.get("name"):
+#             user = User().update(current_user["_id"], user["name"])
+#             return jsonify({
+#                 "message": "successfully updated account",
+#                 "data": user
+#             }), 201
+#         return {
+#             "message": "Invalid data, you can only update your account name!",
+#             "data": None,
+#             "error": "Bad Request"
+#         }, 400  
+#     except Exception as e:
+#         return jsonify({
+#             "message": "failed to update account",
+#             "error": str(e),
+#             "data": None
+#         }), 400
+
